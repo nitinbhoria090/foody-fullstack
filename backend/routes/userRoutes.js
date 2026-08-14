a42beef2a74229a9a3f3c09bbb49e801198f9581
nitinbhoria090 <nitinbhoria090@gmail.com>
2026-08-14 21:30:00 +0530

    14-8

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
 
-const { getProfile } = require("../controllers/userController");
+const { getProfile } = require("../controllers/usercontroller");
 
 router.get("/profile", authMiddleware, getProfile);
 
