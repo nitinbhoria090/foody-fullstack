39052dc36da1d19ef5c2ddbecd2a6967ecb7d76d
nitinbhoria090 <nitinbhoria090@gmail.com>
2026-08-15 17:49:48 +0530

    15-8

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,19 +1,41 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//     baseURL: "http://localhost:5000/api"
+// });
+
+// API.interceptors.request.use((config)=>{
+
+//     const token = localStorage.getItem("token");
+
+//     if(token){
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+
+// });
+
+// export default API;
+
 import axios from "axios";
 
+// 1. Live aur Local donon URLs ko support karne ke liye environment variable setup karein
+const BASE_URL = import.meta.env.VITE_APP_API_URL || process.env.VITE_APP_API_URL || "http://localhost:5000";
+
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    // 2. Base URL ke peeche automatically /api jod dega
+    baseURL: `${BASE_URL}/api`
 });
 
-API.interceptors.request.use((config)=>{
-
+API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
-    if(token){
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-
 });
 
 export default API;
